808689f65cf735d6fc71e22c174a2ba4d848b19a
mandelatuks <manderat206@gmail.com>
2022-04-05 15:47:58 +0200

    add interaction to the humberger menu of mobile

---
+++ main.js
@@ -0,0 +1,25 @@
+const humberg = document.querySelector('.navigation');
+const navList = document.querySelector('.nav-list');
+const times = document.querySelector('.times');
+const navLink = document.querySelectorAll('.nav-link');
+function closeNavItems() {
+  navList.classList.replace('open', 'nav-list');
+  navList.style.display = '';
+}
+humberg.addEventListener('click', () => {
+  navList.classList.replace('nav-list', 'open');
+  times.classList.add('fa-times');
+  navList.style.display = 'block';
+});
+
+times.addEventListener('click', () => {
+  closeNavItems();
+});
+
+navLink.forEach((link) => {
+  link.addEventListener('click', () => {
+    if (window.innerWidth > 768) {
+      closeNavItems();
+    }
+  });
+});
